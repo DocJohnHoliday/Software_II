5e217f1aa092da3d7b36fe162a1ea33e9363af0a
Watkins <zwatki1@wgu.edu>
2022-03-26 12:12:19 -0400

    Created Main From scene and controller

--- src/Controller/Login_Controller.java
+++ src/Controller/Login_Controller.java
@@ -6,19 +6,22 @@ import Warnings.Login_Warnings;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 
 
-public class LoginController implements Initializable {
+public class Login_Controller implements Initializable {
     Stage stage;
     Parent scene;
 
@@ -28,9 +31,8 @@ public class LoginController implements Initializable {
     public TextField Password;
     public TextField Location;
 
-    public Button loginButton;
 
-    public void loginButton(ActionEvent actionEvent) throws SQLException {
+    public void loginButton(ActionEvent actionEvent) throws SQLException, IOException {
 
         String textName = Name.getText();
         String textPassword = Password.getText();
@@ -44,7 +46,16 @@ public class LoginController implements Initializable {
                    } else {
 
                        if (U.getName().equals(textName) && U.getPassword().equals(textPassword)) {
-                           System.out.println("Y");
+
+                           FXMLLoader loader = new FXMLLoader();
+                           loader.setLocation(getClass().getResource("/view/Main_Form.fxml"));
+                           loader.load();
+
+                           stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+                           Parent scene = loader.getRoot();
+                           stage.setScene(new Scene(scene));
+                           stage.show();
+
                        } else {
                            Login_Warnings.incorrectLogin();
                        }
